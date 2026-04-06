a26f7cf3f338296c67f95eb87b1b395cc7ae775b
shrutikale24 <shrutikale930@gmail.com>
2026-04-06 14:33:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("contactForm")?.addEventListener("submit",function(e){
+
+e.preventDefault()
+
+let name=document.getElementById("name").value
+let email=document.getElementById("email").value
+let message=document.getElementById("message").value
+
+if(name==""||email==""||message==""){
+
+document.getElementById("formMessage").innerText="Please fill all fields"
+
+return
+
+}
+
+document.getElementById("formMessage").innerText="Message sent successfully!"
+
+})
